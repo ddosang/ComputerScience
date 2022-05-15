4e244af860ecd07b7e60f95726b51353bde04e3c
ddosang <40856360+ddosang@users.noreply.github.com>
2022-05-15 12:33:02 +0900

    Create 1-3-06.c

---
+++ C로 쓴 자료구조론/CH01/1-3-06.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+int main(void) {
+    int n;
+    int sum = 0;
+    
+    printf("양수 하나를 입력하세요 : ");
+    scanf("%d", &n);
+    
+    if (n < 1) {
+        printf("양수가 아닙니다...\n");
+        return - 1;
+    }
+    
+    for(int i = 1; i < (int)sqrt(n) + 1; i++) {
+        if (n % i == 0) {
+            sum += i;
+            sum += n / i;
+        }
+    }
+    
+    printf("%d 의 모든 제수의 합은 %d 입니다.\n", n, sum);
+    if (sum == n) {
+        printf("%d 와 모든 제수의 합이 같습니다.\n", n);
+    } else {
+        printf("%d 와 모든 제수의 합이 다릅니다.\n", n);
+    }
+    
+    return 0;
+}
