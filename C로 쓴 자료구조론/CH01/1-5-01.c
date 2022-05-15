595aa374a80ce64f1ca2bd54208b922edb058b9e
ddosang <40856360+ddosang@users.noreply.github.com>
2022-05-15 15:06:03 +0900

    Create 1-5-01.c

---
+++ C로 쓴 자료구조론/CH01/1-5-01.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAX_SIZE 20
+
+int main(void) {
+    
+    // n에는 최고 차수를 저장.
+    // a 에 다항식의 계수를 저장하되, 차수가 작은 것부터 저장한다.
+//    int n = 2;
+//    int a[MAX_SIZE] = {1, 2, 1, 0}; // x^2 + 2 * x + 1
+    int n;
+    int a[MAX_SIZE];
+    int x;
+    int count;
+    
+    
+    printf("다항식 최고차항의 계수를 입력하세요 (최대 19) : ");
+    scanf("%d", &n); count++;
+    
+    printf("다항식의 계수를 차수가 큰 것부터 순서대로 입력하세요 : ");
+    for(int i = n; i >= 0; i--) {
+        scanf("%d", &a[i]); count++;
+    }
+    
+    printf("x 값을 입력하세요 : ");
+    scanf("%d", &x);
+    
+    int cal = a[n]; count++;
+    for (int i = n - 1; i >= 0; i--) {
+        cal = cal * x + a[i]; count++;
+    }
+    
+    printf("계산 결과 : %d\n", cal);
+    
+    return 0;
+}
